4c8ef29ed1297ab2cfdd2d03a3a96752ed341964
FKAV64 <fomavalerio@gmail.com>
2026-04-27 01:58:19 +0300

    chore(frontend): bootstrap angular 17 with tailwind, material, jest, pwa

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:3000/api',
+  binanceWsUrl: 'wss://stream.binance.com:9443/stream',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:3000/api',
+  binanceWsUrl: 'wss://stream.binance.com:9443/stream',
+};
